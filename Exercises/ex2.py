1c689a7f76ae0fd21fc9343ac118b4eb0331e48d
wabi-sabi-meraki <jenga.meraki@gmail.com>
2020-11-05 18:29:03 +0100

    Aprendi que el # es para hacer comentarios y no ordenes.

---
+++ Exercises/ex2.py
@@ -0,0 +1,9 @@
+# Comento, esto puedes leerlo mas tarde.
+# Cualquier cosa despues del# lo ignora python.
+
+print("Yo tengo que hacer el codigo asi")# y comento despues es ignorante
+
+# Tu puedes solamente usar el comentario "desabilitarlo" o comentar fuera el codigo:
+# print("Esto no arranca.")
+
+print ("Esto va a arrancar.")
