7cb14c9bd325dcf57872ef7630353ac1c519c4bc
wabi-sabi-meraki <jenga.meraki@gmail.com>
2020-11-05 18:50:11 +0100

    En estos momentos eh aprendido a usar operaciones matematicas simples.

---
+++ Exercises/ex3.py
@@ -0,0 +1,23 @@
+print("Ahora voy a contar mis pollos:")
+
+print("Gallinas", 25 + 30 / 6)
+print("Gallos", 100 - 25 * 3 % 4)
+
+print("Ahora voy a contar los huevos")
+
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+print("Es la verdad 3 + 2 < 5 - 7?")
+
+print(3 + 2 < 5-7)
+
+print("Cuanto es 3 + 2?", 3 + 2)
+print("Cuanto es 5 - 7?", 5 - 7)
+
+print("Oh, eso es falso.")
+
+print("Que tal algo mas.")
+
+print("Es mayor?", 5 > -2)
+print("Es mayor o igual?", 5 >= -2)
+print("Es menos o igual?", 5 <= -2)
