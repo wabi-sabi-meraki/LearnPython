b3f61ba00e52375d16c9a84689003b48dd096a99
wabi-sabi-meraki <jenga.meraki@gmail.com>
2020-11-06 17:38:32 +0100

    Aprendi a ponerle valor a variables que despues las utilizare en mi comandos.

---
+++ Exercises/ex4.py
@@ -0,0 +1,16 @@
+autos = 100
+espacio en el auto = 4.0
+conductores = 30
+pasajeros = 90
+no hay conductores = autos - conductores
+autos_conductores = autos_conductores
+carnet_capacidad = conductores_autos * espacio_en_el_auto
+promedio_pasajeros_en_el_auto = pasajero / autos_conductores
+
+
+print("Aqui hay" , autos,"promedio de autos.")
+print("Solo hay", conductores, "pormedio de conductores.")
+print("Puede ser", no hay conductores, "no hay autos hoy.")
+print("Podemos trasportalos", carne_conducir, "gente hoy.")
+print("Nosotros tenemos", pasajeros, "carne de conducir hoy.")
+print("Necesitamos sobre poner", promedio_pasajeros_para_autos, "en cada auto.")
