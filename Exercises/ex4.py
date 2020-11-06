e4e088a3ad278ddcdd591e21afb219dfecf9477f
wabi-sabi-meraki <jenga.meraki@gmail.com>
2020-11-06 18:08:35 +0100

    Nota: es importante usar bien tus variables y que las escribas exactamente como las pusiste anteriormente para darles valor, si te equivocas u omites puntos, letras etc, te marcara como error. tambien es importante que el espacio que necesitas entre palabras esten acompañadas con gion bajo. por ejemplo: te_quiero.

--- Exercises/ex4.py
+++ Exercises/ex4.py
@@ -1,16 +1,16 @@
 autos = 100
-espacio en el auto = 4.0
+espacio_en_el_auto = 4.0
 conductores = 30
 pasajeros = 90
-no hay conductores = autos - conductores
-autos_conductores = autos_conductores
-carnet_capacidad = conductores_autos * espacio_en_el_auto
-promedio_pasajeros_en_el_auto = pasajero / autos_conductores
+no_hay_conductores = autos - conductores
+autos_conductores = conductores
+carnet_conducir = autos_conductores * espacio_en_el_auto
+promedio_pasajeros_en_el_auto = pasajeros / autos_conductores
 
 
 print("Aqui hay" , autos,"promedio de autos.")
 print("Solo hay", conductores, "pormedio de conductores.")
-print("Puede ser", no hay conductores, "no hay autos hoy.")
-print("Podemos trasportalos", carne_conducir, "gente hoy.")
-print("Nosotros tenemos", pasajeros, "carne de conducir hoy.")
-print("Necesitamos sobre poner", promedio_pasajeros_para_autos, "en cada auto.")
+print("Puede ser", no_hay_conductores, "no hay autos hoy.")
+print("Podemos trasportalos", carnet_conducir, "gente hoy.")
+print("Nosotros tenemos", pasajeros, "carnet de conducir hoy.")
+print("Necesitamos sobre poner", promedio_pasajeros_en_el_auto, "en cada auto.")
