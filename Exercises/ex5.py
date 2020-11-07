49e130889450a9f67fe183a8b2fdd261695d4150
wabi-sabi-meraki <jenga.meraki@gmail.com>
2020-11-07 12:51:21 +0100

    Eh aprendido que despues de dar las variantes, cuando doy las ordenes debo colocar una f al incio. puedes poner variantes despues de hacer las ordenes si es que te faltaron algunos datos

---
+++ Exercises/ex5.py
@@ -0,0 +1,18 @@
+mi_nombre = 'Jennifer Galvan'
+mi_edad = 35 # no a la linea
+mi_peso = 50 # kilos
+mi_estatura = 160 # centimetros
+mis_ojos = 'cafe con verde'
+mis_dientes = 'blancos'
+mi_cabello  = 'cafe'
+
+print(f"Vamos hablar de {mi_nombre}.")
+print(f"De {mi_estatura} altura media.")
+print(f"De {mi_peso} es un peso saludable.")
+print("Actualmente tengo un peso saludable.")
+print(f"Ahora ojos {mis_ojos}  y cabello {mi_cabello}.")
+print(f"Esos dientes son usualmente {mis_dientes} dependen del cafe.")
+
+# esta linea es un truco, trata de escribir exactamente igual
+total = mi_edad + mi_peso + mi_estatura
+print(f"esa es {mi_edad}, {mi_peso}, y {mi_estatura} es un {total}.")
