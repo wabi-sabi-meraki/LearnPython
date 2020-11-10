3d916441a2387251da9cca5415e067612953e100
wabi-sabi-meraki <jenga.meraki@gmail.com>
2020-11-10 17:36:28 +0100

    es importante tener encuenta que hay palabras reservadas que sin ellas te maracaran error, entre las reservadas estan la False  y la end.

---
+++ Exercises/ex7.py
@@ -0,0 +1,21 @@
+print("Ikal tiene una pequeña lampara.")
+print("La tela era blanca {}.".format('nieve'))
+print("Y a donde vaya Ikal.")
+print("." * 10) # ¿Adonde ira Ikal?
+
+y1 = "C"
+y2 = "h"
+y3 = "e"
+y4 = "e"
+y5 = "s"
+y6 = "e"
+y7 = "B"
+y8 = "u"
+y9 = "r"
+y10 = "g"
+y11 = "e"
+y12 = "r"
+
+# mira y = '' y la y. tratamos de remover si lo mira que pasa
+print(y1 + y2 + y3 + y4 + y5 + y6, end=' ')
+print(y7 + y8 + y9 + y10 + y11 + y12)
