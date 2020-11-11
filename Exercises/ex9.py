89e8796cde12201ed9972399f1351d8d6b53f9ca
wabi-sabi-meraki <jenga.meraki@gmail.com>
2020-11-11 17:03:56 +0100

    Aprendi, que si imprimimos y ponemos (""" y escribes todo lo que uno quiera con espacios y cambiando de renglon, cuando terminas de escribir tienes que terminar en un renglon nuevo las""") y se imprimira correctamente. tambien es imporante que si quieremos que una lista se ponga en horizontal es importante poner: \n, en cada objeto de tus listas. la diferencia entre el ejercico 7 y este respecto a las listas es que en el ciente debemos darle un valor y ese valor entre " y cuando pedimos que imprima sumando cada una de los valores que dimos los unira.

---
+++ Exercises/ex9.py
@@ -0,0 +1,14 @@
+# Aqui hay algo extraño, recuerdo exactamente que podria ser.
+
+dias = "lunes Martes Miercoles Jueves Viernes Sabado Domingo"
+meses = "Ene\nFeb\nMar\nAbr\nMay\nJun\nJul\nAgos\nSep\nOct\nNov\nDic"
+
+print("Aqui los dias: ",dias)
+print("Aqui los meses: ",meses)
+
+print("""
+Aqui hay algo.
+Con tres dobles cuestiones.
+Sera posible que nos guste tu tipo.
+Incluso 4 lineas necesitamos, o 5, o 6.
+""")
