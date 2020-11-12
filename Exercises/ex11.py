6255d1e4952841b48e143968494515ec789180f1
wabi-sabi-meraki <jenga.meraki@gmail.com>
2020-11-12 16:32:48 +0100

    cuando ponesmos un input en el codigo, permite que contestes en la terminal la pregunta que te hacen o lo que te pidan que respondas.

---
+++ Exercises/ex11.py
@@ -0,0 +1,8 @@
+print("Cuantos años tienes?", end='')
+años = input()
+print("Cual es tu estatura?", end='')
+estatura = input()
+print("Cuanto pesas?", end='')
+peso = input()
+
+print(f"Entonces, tu tienes {años} edad, {estatura} estatura y {peso} peso.")
