d55e6a1abd3349bfff5cd28da433fa75d54f1881
wabi-sabi-meraki <jenga.meraki@gmail.com>
2020-11-12 16:19:45 +0100

    aprendi a utilizar\t para darle formato a las listas y se le puede poner una viñeta como esta*

---
+++ Exercises/ex10.py
@@ -0,0 +1,15 @@
+tabby_gato = "\tyo doy espacio."
+persian_gato = "yo divido\nen la linea."
+blackslash_gato = "yo \\ soy un \\ gato."
+
+gordo_gato = """
+yo hare una lista:
+\t* Comida para gatos
+\t* Pescado
+\t* hieba para gatos\n\t* Grasa
+"""
+
+print(tabby_gato)
+print(persian_gato)
+print(blackslash_gato)
+print(gordo_gato)
