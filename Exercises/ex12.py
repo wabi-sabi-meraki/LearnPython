92ff8decacae5bd79da88b77c5d5545367f9347a
wabi-sabi-meraki <jenga.meraki@gmail.com>
2020-11-13 16:36:22 +0100

    en este ejercicio,simplificamos las ordenes, dandole valor primero y despues imprimimos, en los valores le damos un sig con el input.

---
+++ Exercises/ex12.py
@@ -0,0 +1,5 @@
+edad = input("Cuantos años tienes? ")
+altura = input("Cual es tu altura? ")
+peso = input("Cual es tu peso? ")
+
+print(f"Entonces, tienes {edad} edad, {altura} altura y {peso} peso.")
