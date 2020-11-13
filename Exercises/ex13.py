de302a71735fd28358703967ea06b05f42fd050c
wabi-sabi-meraki <jenga.meraki@gmail.com>
2020-11-13 17:15:27 +0100

    en este ejercicio es importante que al abrir el documento en la terminal, debemos abrirlo, escribiendo 3 variables. cuando en nuestro codigo ordenamos el argv entoces hay que escribir las variables

---
+++ Exercises/ex13.py
@@ -0,0 +1,8 @@
+from sys import argv
+# lee en WYSS seccion para poder escribir en
+script, primera, segunda, tercera = argv
+
+print("El script es llamar:", script)
+print("Tu primera variable es:", primera)
+print("Tu segunda variable es:", segunda)
+print("Tu tercera variable es:", tercera)
