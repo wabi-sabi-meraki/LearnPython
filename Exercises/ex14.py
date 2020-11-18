87d5aed67c09ac6f9fc24fffa2e4d7f5d36645a6
wabi-sabi-meraki <jenga.meraki@gmail.com>
2020-11-18 13:12:52 +0100

    cuando ordenamos un script con argv debo escribir lo que espesifico para que el archivo se abra sin error.

---
+++ Exercises/ex14.py
@@ -0,0 +1,123 @@
+from sys import argv
+
+script, tu_nombre = argv
+prompt = '> '
+
+print(f"Hola {tu_nombre}, yo {script} escribe.")
+print("Me gustaria hacerte algunas preguntas.")
+print(f"Te gusto {tu_nombre}?")
+gusto = input(prompt)
+
+print(f"Donde vivies {tu_nombre}?")
+vives = input(prompt)
+
+print("Que tipo de computadora tienes?")
+computadora = input(prompt)
+
+print(f"""
+Muy bien, entoces tu dices {gusto} hacerca de mi.
+Tu vives en {vives}. No estoy seguro de donde es.
+Y tienes una {computadora} computadora. Bien.
+""")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print("la cigüeña es cigüeña porque fue al baño")
